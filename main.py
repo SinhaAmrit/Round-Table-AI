bc7aaf78085c18a3f3f014d1864a5259c1fe7577
Amrit Sinha <purpleplots@gmail.com>
2023-10-20 00:24:38 +0530

    Auth UI done

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index_page():
     return render_template("index.html")
 
+@app.route("/signin")
+def signin_page():
+    return render_template("auth/signin.html")
+
+@app.route("/signup")
+def signup_page():
+    return render_template("auth/signup.html")
+
+@app.route("/dashboard")
+def dashboard_page():
+    return render_template("/dashboard.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 app.run(debug=True)
