8a3bce89edc998b363bbc2e3b342b7d910fa3603
SinhaAmrit <purpleplots@gmail.com>
2023-12-13 17:25:19 +0530

    Revert "basic layout done"
    This reverts commit 9d6c34485695c3018147d69a7651d8dd02e2314b.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
+
 
 @app.route("/")
 def index_page():
@@ -10,30 +9,18 @@ def index_page():
 
 @app.route("/signin")
 def signin_page():
-    return render_template("auth/signin.html", title="Sign In")
+    return render_template("auth/signin.html")
 
 @app.route("/signup")
 def signup_page():
-    return render_template("auth/signup.html", title="Sign Up")
+    return render_template("auth/signup.html")
 
 @app.route("/dashboard")
 def dashboard_page():
-    return render_template("/dashboard.html", title = "Dashboard", greeting = greeting())
+    return render_template("/dashboard.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', title = "404"), 404
-
-
-
-def greeting():
-    current_time = datetime.now().time()
-    if current_time.hour < 12:
-        return "Good Morning"
-    elif current_time.hour < 16:
-        return "Good Afternoon"
-    else:
-        return "Good Evening"
-
+    return render_template('404.html'), 404
 
 app.run(debug=True)
