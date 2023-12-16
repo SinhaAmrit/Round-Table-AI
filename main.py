98c84315852025dfbcdc1fb6b07dce73ee68c3b9
SinhaAmrit <purpleplots@gmail.com>
2023-12-16 12:41:20 +0530

    removed unnecessary files

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def dashboard_page():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("404.html", title="SPage Not Found"), 404
 
 
 app.run(debug=True)
--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-import src.comment_prediction
