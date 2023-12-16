c27737ca20a271b54dc54182477fa9bd77265a43
SinhaAmrit <purpleplots@gmail.com>
2023-12-16 12:12:28 +0530

    auth frontend done

--- main.py
+++ main.py
@@ -7,20 +7,30 @@ app = Flask(__name__)
 def index_page():
     return render_template("index.html")
 
+
 @app.route("/signin")
 def signin_page():
-    return render_template("auth/signin.html")
+    return render_template("auth/signin.html", title="Sign In")
+
 
 @app.route("/signup")
 def signup_page():
-    return render_template("auth/signup.html")
+    return render_template("auth/signup.html", title="Sign Up")
+
+
+@app.route("/recover-password")
+def recover_password():
+    return render_template("auth/recover-password.html", title="Recover Password")
+
 
 @app.route("/dashboard")
 def dashboard_page():
-    return render_template("/dashboard.html")
+    return render_template("/dashboard.html", title="Dashboard")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 app.run(debug=True)
---
+++ static/js/anim.js
@@ -0,0 +1,5 @@
+Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+});
