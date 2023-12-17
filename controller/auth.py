afed5d09eb54631cab512b4348c0984e97c3e631
Ashutosh Singh Thakur <ashutoshsingh999th@gmail.com>
2023-12-17 13:44:23 +0530

    flash message added after navigation bar

--- controller/auth.py
+++ controller/auth.py
@@ -10,70 +10,70 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
+    print("Entering sign_up function")
     if request.method == 'POST':
-
+        print("Handling POST request")
         name = request.form.get('name')
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
         opt_in = 'opt-in' in request.form
-
-        conditions = [
-            (User.query.filter_by(email=email).first(), 'Email already exists.'),
-            (User.query.filter_by(username=username).first(), 'Username already exists.'),
-            (len(email) < 4, 'Invalid email address.'),
-            (len(password) < 8, 'Password must be at least 8 characters.')]
-
-        for condition, error_message in conditions:
-            if condition:
-                flash(error_message, category='error')
+        print("data excepted from form successfully")
+        if not (name and username and email and password):
+            flash('Enter all fields.', category='error')
+            print("User already exist!")
+            return redirect(url_for('auth.sign_up'))
+        else:
+            if User.query.filter_by(email=email).first():
+                flash('Email already exists.', category='error')
+                print("User already exist!")
                 return redirect(url_for('auth.sign_up'))
 
-        else:
-        # All conditions passed, create a new user
-            hashed_password = generate_password_hash(password, method='sha256')
-            new_user = User(email=email, F_name=name, username=username, password=hashed_password, created_at=datetime.utcnow())
+            else:
+                #condition passed, create a new user
+                hashed_password = generate_password_hash(password, method='sha256')
+                new_user = User(email=email, F_name=name, username=username, password_hash=hashed_password, created_at=datetime.utcnow())
+                print("User created successfully!")
 
-        if opt_in:
-            current_user.email = new_user.email
-            current_user.F_name = new_user.F_name
-            current_user.username = new_user.username
-            current_user.password_hash = new_user.password_hash
-
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user, remember=True)
-        flash('Account created!', category='success')
-        return redirect(url_for('views.home'))
-
-    return render_template("auth/signup.html", title="Sign Up")
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Account created!', category='success')
+                print('session created successfully!')
+                return redirect(url_for('views.home'))
+    print("Handling GET")
+    return render_template("auth/signup.html", title="Sign Up" )
 
 
 @auth.route('/signin', methods=['GET', 'POST'])
 def sign_in():
+    print("Entering sign_in function")
     if request.method == 'POST':
-
-        identifier = request.form.get('identifier')  # This can be either email or username
+        print("Handling POST request")
+        email = request.form.get('email')
         password = request.form.get('password')
-
+        print("data excepted from form successfully")
         # Check if the identifier is an email
-        user = User.query.filter_by(email=identifier).first()
-
+        user = User.query.filter_by(email=email).first()
+        print("data excepted from form successfully")
         # If the identifier is not an email, check if it's a username
         if not user:
-            user = User.query.filter_by(username=identifier).first()
+            user = User.query.filter_by(username=email).first()
 
         if user:
             # User found, check the password
-            if check_password_hash(user.password, password):
+            print("User found")
+            if check_password_hash(user.password_hash, password):
+                print("Password matched")
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
+            print("User not found")
             flash('User does not exist.', category='error')
-
+    print("Handling GET")
     return render_template("auth/signin.html", title="Sign In")
 
 
@@ -87,4 +87,5 @@ def recover_password():
 def sign_out():
     current_user.last_seen = datetime.utcnow()
     logout_user()
+    flash('Logged out successfully!', category='success')
     return redirect(url_for('auth.sign_in'))
