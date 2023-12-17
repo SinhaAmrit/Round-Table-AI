f38f75add919fefd2ef5ca61999d335b42d4857f
Ashutosh Singh Thakur <ashutoshsingh999th@gmail.com>
2023-12-17 19:41:43 +0530

    added action and methods in forms of html files

--- controller/views.py
+++ controller/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Discussion
+from . import db
+from datetime import datetime
+from flask_login import login_required, current_user
+
 
 views = Blueprint('views', __name__)
 
-
+                                # HOME
+#========================================================================================================
 @views.route('/')
 def home():
-    return render_template("index.html")
+        return render_template('index.html')
+#========================================================================================================
