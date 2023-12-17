6ac9cb77db3d83623cc76e849580637892e223e9
Ashutosh Singh Thakur <ashutoshsingh999th@gmail.com>
2023-12-17 19:40:24 +0530

    added update_at column in Discussion

--- controller/models.py
+++ controller/models.py
@@ -1,17 +1,18 @@
 from . import db
 import uuid
+from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 
-
+                                # USER
+#========================================================================================================
 class User(db.Model, UserMixin):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    F_name = db.Column(db.String(16))
-    L_name = db.Column(db.String(16))
+    name = db.Column(db.String(16))
     email = db.Column(db.String(50), unique=True, nullable=False)
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     secret_key = db.Column(db.String(125))
@@ -32,26 +33,32 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-
+#========================================================================================================
+                                # DISCUSSION
+#========================================================================================================
 class Discussion(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.TEXT)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
-    deleted_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP, nullable=True, default=None)
+    deleted_at = db.Column(db.TIMESTAMP, nullable=True, default=None)
+    image_id = db.Column(UUID(as_uuid=True), nullable=True, default=None)
     slug = db.Column(db.String(50))
     summary = db.Column(db.TEXT)
-    archived = db.Column(db.Boolean)
-
-
+    archived = db.Column(db.Boolean, default=False)
+    user = db.relationship('User')
+#========================================================================================================
+                                # CATEGORY
+#========================================================================================================
 class Category(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
-
-
+#========================================================================================================
+                                # CONNECTED-ACCOUNT
+#========================================================================================================
 class ConnAccount(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     github = db.Column(db.String(50))
@@ -60,8 +67,9 @@ class ConnAccount(db.Model):
     facebook = db.Column(db.String(50))
     twitter = db.Column(db.String(50))
     slak = db.Column(db.String(50))
-
-
+#========================================================================================================
+                                # NOTIFICATION
+#========================================================================================================
 class Notification(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
@@ -69,8 +77,9 @@ class Notification(db.Model):
     data = db.Column(db.TEXT)
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     read_at = db.Column(db.TIMESTAMP)
-
-
+#========================================================================================================
+                                # REPLY
+#========================================================================================================
 class Reply(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
@@ -78,8 +87,9 @@ class Reply(db.Model):
     content = db.Column(db.TEXT)
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     deleted_at = db.Column(db.TIMESTAMP)
-
-
+#========================================================================================================
+                                # HISTORY
+#========================================================================================================
 class History(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
@@ -88,15 +98,18 @@ class History(db.Model):
     reply_id = db.Column(UUID(as_uuid=True), db.ForeignKey('reply.id'))
     visited_at = db.Column(db.TIMESTAMP)
     deleted_at = db.Column(db.TIMESTAMP)
-
-
+#========================================================================================================
+                                # DISCUSSION-REPLY
+#========================================================================================================
 class DiscussReply(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     discussion_id = db.Column(UUID(as_uuid=True), db.ForeignKey('discussion.id'))
     reply_id = db.Column(UUID(as_uuid=True), db.ForeignKey('reply.id'))
-
-
+#========================================================================================================
+                                # DISCUSSION-CATEGORY
+#========================================================================================================
 class DiscussCategory(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     discussion_id = db.Column(UUID(as_uuid=True), db.ForeignKey('discussion.id'))
     category_id = db.Column(UUID(as_uuid=True), db.ForeignKey('category.id'))
+#========================================================================================================
