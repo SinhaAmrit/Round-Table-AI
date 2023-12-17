1a15bd752e803ac3fdd741af5cd04d2da606f4f1
SinhaAmrit <purpleplots@gmail.com>
2023-12-17 11:01:29 +0530

    badges-list and user-list page added

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ def index_page():
     return render_template("index.html")
 
 
-
-
 # =======================================Auth Route Start=======================================
 @app.route("/signin")
 def signin_page():
@@ -37,13 +35,24 @@ def recover_password():
 def questions_page():
     return render_template("discussion/questions.html", title="Questions")
 
+
 @app.route("/question")
 def full_question():
     return render_template("/discussion/question.html", title="Discussion")
 
+
 @app.route("/ask-question")
 def ask_question():
     return render_template("/discussion/ask-question.html", title="Ask Question")
+
+
+@app.route("/tags-list")
+def tags_list():
+    return render_template("/discussion/tags-list.html", title="Tags List")
+
+@app.route("/category")
+def category():
+    return render_template("/discussion/category.html", title="Categories")
 
 
 # ====================================Discussion Route End====================================
@@ -57,5 +66,20 @@ def page_not_found(e):
 
 
 # ======================================Error Route End======================================
+
+# =====================================Extracts Route Start=====================================
+
+
+@app.route("/user-list")
+def user_list():
+    return render_template("/extracts/user-list.html", title="User List")
+
+@app.route("/badges-list")
+def badges_list():
+    return render_template("/extracts/badges-list.html", title="Badges List")
+
+
+# =====================================Extracts Route End=====================================
+
 
 app.run(debug=True)
