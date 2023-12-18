66085ee14f1335ec36e905430d2f67216a3cedfd
Ashutosh Singh Thakur <ashutoshsingh999th@gmail.com>
2023-12-18 19:39:22 +0530

    is_signed passed to home page

--- app/views.py
+++ app/views.py
@@ -10,7 +10,9 @@ views = Blueprint('views', __name__)
 #========================================================================================================
 @views.route('/')
 def home():
-        return render_template('index.html')
+    is_signed = current_user.is_authenticated
+    print(is_signed)
+    return render_template('index.html', is_signed=is_signed)
 #========================================================================================================
                                 # Page Not Found
 #========================================================================================================
@@ -18,3 +20,6 @@ def home():
 def page_not_found(e):
     return render_template("404.html", title="Page Not Found"), 404
 #========================================================================================================
+@views.errorhandler(500)
+def server_not_found(e):
+    return render_template("500.html", title="Server Not Found"),
