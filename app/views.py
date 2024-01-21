68ba69c8b852dde0c0db763c5f8c50c202be10c4
Ashutosh Singh Thakur <ashutoshsingh999th@gmail.com>
2024-01-21 19:30:41 +0530

    change route of home

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User, Discussion
+from .models import User, Question
 from . import db
 from datetime import datetime
 from flask_login import login_required, current_user
@@ -8,7 +8,7 @@ views = Blueprint('views', __name__)
 
                                 # HOME
 #========================================================================================================
-@views.route('/')
+@views.route('/home')
 def home():
     return render_template('index.html',title="Home")
 #========================================================================================================
@@ -17,6 +17,8 @@ def home():
 @views.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html", title="Page Not Found"), 404
+#========================================================================================================
+                                # Server Not Found
 #========================================================================================================
 @views.errorhandler(500)
 def server_not_found(e):
